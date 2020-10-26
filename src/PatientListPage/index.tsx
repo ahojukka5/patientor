f867f67fa21885630381e09a745ab5382140cc83
Jukka Aho <ahojukka5@gmail.com>
2020-10-26 17:21:32 +0700

    useHistory from react-router-dom

--- src/PatientListPage/index.tsx
+++ src/PatientListPage/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import axios from "axios";
-import { Container, Table, Button } from "semantic-ui-react";
+import { useHistory } from 'react-router-dom';
+import { Container, Table, Button } from 'semantic-ui-react';
 
 import { PatientFormValues } from "../AddPatientModal/AddPatientForm";
 import AddPatientModal from "../AddPatientModal";
