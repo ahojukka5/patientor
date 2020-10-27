67b8a893aa4697d24e4289bac8bc365030971f05
Jukka Aho <ahojukka5@gmail.com>
2020-10-27 21:04:11 +0700

    Entry details for each type

--- src/PatientDetailPage/index.tsx
+++ src/PatientDetailPage/index.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useStateValue, addPatient } from '../state';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Header, Item, Divider, Loader, Dimmer } from 'semantic-ui-react';
+
 import { apiBaseUrl } from '../constants';
-import { Patient, Gender, Entry, Diagnosis } from '../types';
+import {
+  Patient,
+  Gender,
+  Entry,
+  HospitalEntry,
+  OccupationalHealthcareEntry,
+  HealthCheckEntry,
+} from '../types';
+import HealthRatingBar from '../components/HealthRatingBar';
 
 const GenderIcon: React.FC<{ gender: Gender }> = ({ gender }) => {
   switch (gender) {
@@ -19,31 +28,118 @@ const GenderIcon: React.FC<{ gender: Gender }> = ({ gender }) => {
   }
 };
 
-const OneEntry: React.FC<Entry> = (entry) => {
-  // console.log(entry);
+const HospitalEntryDetails: React.FC<HospitalEntry> = (entry) => {
   const [{ diagnoses }] = useStateValue();
-  // console.log(diagnoses);
+  console.log(entry);
+  const diagstr = entry.diagnosisCodes
+    ?.map((code) => `${code} (${diagnoses[code].name})`)
+    .join(', ');
   return (
-    <div>
-      <div>
-        {entry.date} {entry.description}
-      </div>
-      <ul>
-        {entry.diagnosisCodes?.map((code: Diagnosis['code']) => (
-          <li key={code}>{code} {diagnoses[code].name}</li>
-        ))}
-      </ul>
-    </div>
+    <Item.Content>
+      <Item.Header as="h3">
+        <Icon name="stethoscope" />
+        {entry.date} Hospital at {entry.specialist}
+      </Item.Header>
+      <Item.Description>
+        <b>Diagnoses:</b> {diagstr}
+      </Item.Description>
+      <Item.Description>
+        <b>Description:</b> {entry.description}
+      </Item.Description>
+      {entry.discharge ? (
+        <Item.Description>
+          <b>Discharged</b> at {entry.discharge.date}:{' '}
+          {entry.discharge.criteria}
+        </Item.Description>
+      ) : null}
+    </Item.Content>
   );
+};
+
+const OccupationalHealthcareEntryDetails: React.FC<OccupationalHealthcareEntry> = (
+  entry
+) => {
+  const [{ diagnoses }] = useStateValue();
+  console.log(entry);
+  const diagstr = entry.diagnosisCodes
+    ?.map((code) => `${code} (${diagnoses[code].name})`)
+    .join(', ');
+  return (
+    <Item.Content>
+      <Item.Header as="h3">
+        <Icon name="stethoscope" />
+        {entry.date} Occupational healthcare at {entry.specialist}
+      </Item.Header>
+      {entry.diagnosisCodes ? (
+        <Item.Description>
+          <b>Diagnoses:</b> {diagstr}
+        </Item.Description>
+      ) : null}
+      <Item.Description>
+        <b>Description:</b> {entry.description}{' '}
+        {entry.sickLeave
+          ? `On sick leave from ${entry.sickLeave.startDate} to ${entry.sickLeave.endDate}.`
+          : null}
+      </Item.Description>
+      <Item.Description>
+        <b>Employer:</b> {entry.employerName}
+      </Item.Description>
+    </Item.Content>
+  );
+};
+
+const HealthCheckEntryDetails: React.FC<HealthCheckEntry> = (entry) => {
+  console.log(entry);
+  return (
+    <Item.Content>
+      <Item.Header>
+        <Icon name="user doctor" />
+        {entry.date} Health check at {entry.specialist}
+      </Item.Header>
+      <Item.Description>
+        <b>Description:</b> {entry.description}
+      </Item.Description>
+      <Item.Description>
+        <HealthRatingBar rating={entry.healthCheckRating} showText={true} />
+      </Item.Description>
+    </Item.Content>
+  );
+};
+
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const EntryDetails: React.FC<Entry> = (entry) => {
+  switch (entry.type) {
+    case 'Hospital':
+      return <HospitalEntryDetails {...entry} />;
+    case 'OccupationalHealthcare':
+      return <OccupationalHealthcareEntryDetails {...entry} />;
+    case 'HealthCheck':
+      return <HealthCheckEntryDetails {...entry} />;
+    default:
+      return assertNever(entry);
+  }
 };
 
 const Entries: React.FC<{ entries: Entry[] }> = ({ entries }) => {
   return (
-    <div>
+    <Item.Group>
       {entries.map((entry: Entry) => (
-        <OneEntry key={entry.id} {...entry} />
+        <div key={entry.id}>
+          <Item>
+            <EntryDetails {...entry} />
+          </Item>
+          <Divider />
+        </div>
       ))}
-    </div>
+    </Item.Group>
   );
 };
 
@@ -52,14 +148,12 @@ const PatientDetailPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const patient = patients[id];
 
-  const loading = () => (
-    <span>
-      loading data... <Icon loading name="spinner" />
-    </span>
-  );
-
   if (!patient) {
-    return loading();
+    return (
+      <Dimmer active inverted>
+        <Loader inverted>Loading</Loader>
+      </Dimmer>
+    );
   }
 
   const fetchPatientDetails = async (id: string) => {
@@ -79,13 +173,22 @@ const PatientDetailPage: React.FC = () => {
 
   return (
     <div className="App">
-      <h1>
+      {!patient.ssn ? (
+        <Dimmer active inverted>
+          <Loader inverted>Loading</Loader>
+        </Dimmer>
+      ) : null}
+      <Header as="h1">
         {patient.name} <GenderIcon gender={patient.gender} />
-      </h1>
-      <div>ssn: {patient.ssn || loading()}</div>
+      </Header>
+      <div>ssn: {patient.ssn || 'unknown'}</div>
       <div>occupation: {patient.occupation}</div>
-      <h2>Entries</h2>
-      {patient.entries ? <Entries entries={patient.entries} /> : loading()}
+      <Header as="h2">Entries</Header>
+      {patient.entries ? (
+        <Entries entries={patient.entries} />
+      ) : (
+        'Waiting for data ...'
+      )}
     </div>
   );
 };
