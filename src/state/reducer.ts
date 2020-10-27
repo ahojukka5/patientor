f8dfd2bd63dbe93d046b73fe58b31172eb0f82c1
Jukka Aho <ahojukka5@gmail.com>
2020-10-27 17:50:26 +0700

    Add SET_DIAGNOSIS_LIST to reducer

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -1,19 +1,23 @@
-import { State } from "./state";
-import { Patient } from "../types";
+import { State } from './state';
+import { Patient, Diagnosis } from '../types';
 
 export type Action =
   | {
-      type: "SET_PATIENT_LIST";
+      type: 'SET_PATIENT_LIST';
       payload: Patient[];
     }
   | {
-      type: "ADD_PATIENT";
+      type: 'ADD_PATIENT';
       payload: Patient;
+    }
+  | {
+      type: 'SET_DIAGNOSIS_LIST';
+      payload: Diagnosis[];
     };
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "SET_PATIENT_LIST":
+    case 'SET_PATIENT_LIST':
       return {
         ...state,
         patients: {
@@ -21,17 +25,31 @@ export const reducer = (state: State, action: Action): State => {
             (memo, patient) => ({ ...memo, [patient.id]: patient }),
             {}
           ),
-          ...state.patients
-        }
+          ...state.patients,
+        },
       };
-    case "ADD_PATIENT":
+
+    case 'ADD_PATIENT':
       return {
         ...state,
         patients: {
           ...state.patients,
-          [action.payload.id]: action.payload
-        }
+          [action.payload.id]: action.payload,
+        },
       };
+
+    case 'SET_DIAGNOSIS_LIST':
+      return {
+        ...state,
+        diagnoses: {
+          ...action.payload.reduce(
+            (memo, diagnosis) => ({ ...memo, [diagnosis.code]: diagnosis }),
+            {}
+          ),
+          ...state.diagnoses,
+        },
+      };
+
     default:
       return state;
   }
